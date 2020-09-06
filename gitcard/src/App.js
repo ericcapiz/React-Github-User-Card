f3d87d2e715fcc108870b4081a834d8eef95d996
eric capiz <ericcapiz@gmail.com>
2020-09-05 20:17:39 -0500

    mvp

--- gitcard/src/App.js
+++ gitcard/src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  state={
+    users: {}
+  }
+  
+  componentDidMount(){
+    axios.get('https://api.github.com/users/ericcapiz')
+    .then(res=>{
+      this.setState({users:res.data})
+      console.log("current state", this.state.users);
+
+    })
+    .catch(err=>alert("get request error"))
+  }
+
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={this.state.users.avatar_url}></img>
+      <h2>Name: {this.state.users.name}</h2>
+      <h2>Git Handle: {this.state.users.login}</h2>
+      <h2>Location: {this.state.users.location}</h2>
+      <h2>Number of followers: {this.state.users.following}</h2>
     </div>
   );
+  }
 }
 
 export default App;
